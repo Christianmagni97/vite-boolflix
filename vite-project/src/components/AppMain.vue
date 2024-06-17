7d3ccf710aff1b9f1f074b576c14f78e6dfcc8f8
Christianmagni97 <christianmagni28@gmail.com>
2024-06-17 11:14:39 +0200

    aggiunto api

--- vite-project/src/components/AppMain.vue
+++ vite-project/src/components/AppMain.vue
@@ -1,9 +1,23 @@
 <script>
+import axios from 'axios';
     export default {
         data() {
             return {
             }
         }
+    },
+    methods:{
+        getSearch(){
+            axios.get('https://api.themoviedb.org/3/search/movie&api_key=17cd8953610657dd6b2e079f297555fe')
+            .then((response) => {
+            this.search = response.data
+            console.log();
+            })
+            .catch(function(error) {
+            console.log(error);
+            });
+        },
+
     }
 </script>
 
